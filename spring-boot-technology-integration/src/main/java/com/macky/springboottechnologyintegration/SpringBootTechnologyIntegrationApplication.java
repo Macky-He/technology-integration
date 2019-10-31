429b262c667d702a94509fd8f0e44bdbf45e7455
Macky-He <97462319@qq.com>
2019-10-31 17:16:08 +0800

    init project

---
+++ spring-boot-technology-integration/src/main/java/com/macky/springboottechnologyintegration/SpringBootTechnologyIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.macky.springboottechnologyintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTechnologyIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTechnologyIntegrationApplication.class, args);
+    }
+
+}
---
+++ spring-boot-technology-integration/src/main/java/com/macky/springboottechnologyintegration/controller/HelloJenkins.java
@@ -0,0 +1,20 @@
+package com.macky.springboottechnologyintegration.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Macky
+ * @Title class HelloJenkins
+ * @Description: TODO
+ * @date 2019/10/31 16:40
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloJenkins {
+
+    @RequestMapping("/jenkins")
+    public String helloJenkins(){
+        return "Hello Jenkins!!!";
+    }
+}
